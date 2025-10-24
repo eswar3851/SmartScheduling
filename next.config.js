ee92047262b5a4a3f5380cc98acee65be16a0193
Eswar Gade <eswar.3851@gmail.com>
2025-10-24 14:16:12 -0500

    chore: update tsconfig imports

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
